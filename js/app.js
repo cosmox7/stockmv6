1f29a3f8551c0c7777948c6f4bfb73baa965739d
cosmox7 <sameerfreelancer381@gmail.com>
2026-05-05 17:54:33 +0530

    Delete js directory

--- js/app.js
+++
@@ -1,8 +0,0 @@
-function checkAppLock(password) {
-    // Correct validation logic
-    const validPassword = "correct_password"; // Replace with actual password or validation logic
-    if (password === validPassword) {
-        return true; // Password is valid
-    }
-    return false; // Password is invalid
-}
